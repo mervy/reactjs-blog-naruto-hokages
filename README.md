dc2ed6880889bc6c7f9b9aa520637f65ec699552
Rogério Soares <rgrsoares@yahoo.com.br>
2024-03-22 19:57:55 -0300

    Create header component and add images

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # reactjs-blog-naruto-hokages
+
 Blog com React para listar os Hokages do anime Naruto
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+export const Header = () => {
+    return (
+        <header className="container">
+            <h1>Hokages - Naruto Anime</h1>
+        </header>
+    )
+}
